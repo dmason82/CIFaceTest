2cae8f13654fc667e4fffa8309dca3b3b32a200c
Doug Mason <dougmason@Doug-Masons-MacBook-Pro.local>
2011-10-28 07:20:42 -0700

    Added OCImageView as an overlay UIView subclass since we need to do some custom drawing for testing.

---
+++ VampTest/OCGImageView.h
@@ -0,0 +1,13 @@
+//
+//  OCGImageView.h
+//  VampTest
+//
+//  Created by Doug Mason on 10/28/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OCGImageView : UIView
+@property(nonatomic,retain)NSArray* facialFeatures;
+@end
---
+++ VampTest/OCGImageView.m
@@ -0,0 +1,43 @@
+//
+//  OCGImageView.m
+//  VampTest
+//
+//  Created by Doug Mason on 10/28/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "OCGImageView.h"
+#import "OCGAppDelegate.h"
+@implementation OCGImageView
+@synthesize facialFeatures;
+-(void)drawRect:(CGRect)rect
+{
+    NSDictionary* options = [[NSDictionary alloc] initWithObjectsAndKeys:@"CIDetectorAccuracy",@"CIDetectorAccuracyHigh", nil];
+    UIImage* bg = [(OCGAppDelegate*)[[UIApplication sharedApplication] delegate] image];
+    CIImage* image = [bg CIImage];
+    NSLog(@"%@",image);
+    CIDetector* detector = [CIDetector detectorOfType:CIDetectorTypeFace context:nil options:options];
+    facialFeatures = [detector featuresInImage:image];
+    NSLog(@"%d",[facialFeatures count]);
+    
+    for (CIFaceFeature* feature in facialFeatures) 
+    {
+        NSLog(@"%i",[feature hasMouthPosition]);
+        NSLog(@"%f,%f",feature.mouthPosition.x,feature.mouthPosition.y);
+        NSLog(@"%f,%f",feature.leftEyePosition.x,feature.leftEyePosition.y);
+        CGContextRef context = UIGraphicsGetCurrentContext();
+        UIGraphicsPushContext(context);
+        CGColorSpaceRef colorspace = CGColorSpaceCreateDeviceRGB();
+        CGContextSetLineWidth(context, 3.0);
+        CGFloat components[] = {1.0,0.0,0.0,1.0};
+        CGColorRef color = CGColorCreate(colorspace, components);
+        CGContextSetStrokeColorWithColor(context, color);
+        CGContextMoveToPoint(context, feature.leftEyePosition.x, feature.leftEyePosition.y);
+        CGContextAddLineToPoint(context, feature.rightEyePosition.x, feature.rightEyePosition.y);
+        CGContextStrokePath(context);
+        CGColorSpaceRelease(colorspace);
+        CGColorRelease(color);
+    }
+    [super drawRect:rect];
+}
+@end
