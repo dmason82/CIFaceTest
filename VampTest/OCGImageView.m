91c6e26782af61838c11fbc8d0cf01a0187f4631
Doug Mason <bastardx@Macintosh.(none)>
2011-11-01 06:45:51 -0700

    Changed Accuracy to low for CIDetector Added subviews based on facial feature positions Added Custom Quartz 2d drawing code to verify that drawing on my overlay view works at all.

--- VampTest/OCGImageView.m
+++ VampTest/OCGImageView.m
@@ -12,7 +12,7 @@
 @synthesize facialFeatures;
 -(void)drawRect:(CGRect)rect
 {
-    NSDictionary* options = [[NSDictionary alloc] initWithObjectsAndKeys:@"CIDetectorAccuracy",@"CIDetectorAccuracyHigh", nil];
+    NSDictionary* options = [[NSDictionary alloc] initWithObjectsAndKeys:@"CIDetectorAccuracy",@"CIDetectorAccuracyLow", nil];
     CGImageRef cgim = [[(OCGAppDelegate*)[[UIApplication sharedApplication] delegate] image] CGImage];
     UIImage* im = [[UIImage alloc] initWithCGImage:cgim];
     CIImage* image = [[CIImage alloc] initWithImage:im];
@@ -32,16 +32,41 @@
         CGContextRef context = UIGraphicsGetCurrentContext();
         UIGraphicsPushContext(context);
         CGColorSpaceRef colorspace = CGColorSpaceCreateDeviceRGB();
-        CGContextSetLineWidth(context, 3.0);
+        CGContextSetLineWidth(context, 10.0);
         CGFloat components[] = {1.0,0.0,0.0,1.0};
         CGColorRef color = CGColorCreate(colorspace, components);
         CGContextSetStrokeColorWithColor(context, color);
-        CGContextMoveToPoint(context, feature.leftEyePosition.x, feature.leftEyePosition.y);
-        CGContextAddLineToPoint(context, feature.rightEyePosition.x, feature.rightEyePosition.y);
+        //CGContextMoveToPoint(context, feature.leftEyePosition.x, feature.leftEyePosition.y);
+        //CGContextAddLineToPoint(context, feature.rightEyePosition.x, feature.rightEyePosition.y);
+        CGContextMoveToPoint(context, 0, 0);
+        CGContextAddLineToPoint(context,self.frame.size.width, self.frame.size.height);
         CGContextStrokePath(context);
         CGColorSpaceRelease(colorspace);
         CGColorRelease(color);
+        if([feature hasLeftEyePosition])
+        {
+            UIView* lEye = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 15.0, 15.0)];
+            [lEye setBackgroundColor:[[UIColor redColor] colorWithAlphaComponent:0.2]];
+            [lEye setCenter:feature.leftEyePosition];
+            [self addSubview:lEye];
+        }
+        if([feature hasRightEyePosition])
+        {
+            UIView* rEye = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 15.0, 15.0)];
+            [rEye setBackgroundColor:[[UIColor redColor] colorWithAlphaComponent:0.2]];
+            [rEye setCenter:feature.rightEyePosition];
+            [self addSubview:rEye];
+        }
+        if([feature hasMouthPosition])
+        {
+            UIView* mouth = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 15.0, 15.0)];
+            [mouth setBackgroundColor:[[UIColor greenColor] colorWithAlphaComponent:0.2]];
+            [mouth setCenter:feature.mouthPosition];
+            [self addSubview:mouth];
+        }
     }
-    [super drawRect:rect];
+    [self setBackgroundColor:[UIColor clearColor]];
+    [self setOpaque:YES];
+    [self setHidden:NO];
 }
 @end
